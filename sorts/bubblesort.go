bea39e198b0338dea4ad9877f4b22a55227d6ef5
luccafort <muspellsheim1985@gmail.com>
2020-11-11 20:09:04 +0900

    高速化する道筋が見つからずキャッシュできるところをキャッシュしてみた

--- sorts/bubblesort.go
+++ sorts/bubblesort.go
@@ -5,9 +5,11 @@ package sorts
 
 func bubbleSort(arr []int) []int {
 	swapped := true
+	length := len(arr)-1
 	for swapped {
 		swapped = false
-		for i := 0; i < len(arr)-1; i++ {
+		i := 0
+		for i; i < length; i++ {
 			if arr[i+1] < arr[i] {
 				arr[i+1], arr[i] = arr[i], arr[i+1]
 				swapped = true
